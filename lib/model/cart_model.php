d6474007cc074fd124b2344dc1b459a15d921fb0
Marcus <105409242+Fullcharge13@users.noreply.github.com>
2022-10-30 23:15:21 +0800

    Update cart_model.php (Ref)

--- lib/model/cart_model.php
+++ lib/model/cart_model.php
@@ -5,14 +5,41 @@
     class CartModel {
         public function addToCart() {
           // FILL HERE
+          if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){ 
+            if($_REQUEST['action'] == 'addToCart' && !empty($_REQUEST['id'])){ 
+                $product_id = $_REQUEST['id'];
         }
 
         public function fetchCartItems() {
           // FILL HERE
+          $sqlQ = "SELECT * FROM products WHERE id=?"; 
+          $stmt = $db->prepare($sqlQ); 
+          $stmt->bind_param("i", $db_id); 
+          $db_id = $product_id; 
+          $stmt->execute(); 
+          $result = $stmt->get_result(); 
+          $productRow = $result->fetch_assoc(); 
+  
+          $itemData = array( 
+              'id' => $productRow['id'], 
+              'image' => $productRow['image'], 
+              'name' => $productRow['name'], 
+              'price' => $productRow['price'], 
+              'qty' => 1 
         }
 
         public function updateCartItems() {
           // FILL HERE
+          $redirectURL = $insertItem?'viewCart.php':'index.php'; 
+    }elseif($_REQUEST['action'] == 'updateCartItem' && !empty($_REQUEST['id'])){ 
+        // Update item data in cart 
+        $itemData = array( 
+            'rowid' => $_REQUEST['id'], 
+            'qty' => $_REQUEST['qty'] 
+        ); 
+        $updateItem = $cart->update($itemData); 
         }
     }
 ?>
+
+// Ref : https://www.codexworld.com/simple-php-shopping-cart-using-sessions/
