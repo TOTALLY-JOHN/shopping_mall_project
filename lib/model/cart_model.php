e3fac26d8c644ae8cc8222029e576113b584b52e
Marcus <105409242+Fullcharge13@users.noreply.github.com>
2022-11-06 12:42:01 +0800

    cart_model, addToCart code

--- lib/model/cart_model.php
+++ lib/model/cart_model.php
@@ -4,42 +4,51 @@
 
     class CartModel {
         public function addToCart() {
-          // FILL HERE
-          if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){ 
-            if($_REQUEST['action'] == 'addToCart' && !empty($_REQUEST['id'])){ 
-                $product_id = $_REQUEST['id'];
+          // 장바구니 제품 추가
+          if (isset($_POST['pid'])) {
+            $pid = $_POST['pid'];
+            $pname = $_POST['pname'];
+            $pcategory = $_POST['pcategory'];
+            $poriginalprice = $_POST['poriginalprice'];
+            $pdiscountprice = $_POST['pdiscountprice'];
+            $pimage = $_POST['pimage'];
+            $pstock = $_POST['pstock'];
+            $totalprice = $pdiscountprice * $pstock;
+
+            $stmt = $conn->prepare('SELECT productId FROM cart WHERE productId=?');
+            $stmt->bind_param('s',$pid);
+            $stmt->execute();
+            $res = $stmt->get_result();
+            $r = $res->fetch_assoc();
+            $code = $r['productId'] ?? '';
+
+            if (!$code) {
+              $query = $conn->prepare('INSERT INTO cart (productId, productName, productCategory, productOriginalPrice, productDiscountPrice,productImage, productStock,totalPrice) VALUES (?,?,?,?,?,?,?,?)');
+              $query->bind_param('ssssss',$pid,$pname,$pcategory,$poriginalprice,$pdiscountprice,$pimage,$pstock,$totalprice);
+              $query->execute();
+        
+              echo '<div class="alert alert-success alert-dismissible mt-2">
+                      <button type="button" class="close" data-dismiss="alert">&times;</button>
+                      <strong>Item added to your cart!</strong>
+                    </div>';
+            } else {
+              echo '<div class="alert alert-danger alert-dismissible mt-2">
+                      <button type="button" class="close" data-dismiss="alert">&times;</button>
+                      <strong>Item already added to your cart!</strong>
+                    </div>';
+            }
+          }
+
         }
 
         public function fetchCartItems() {
           // FILL HERE
-          $sqlQ = "SELECT * FROM products WHERE id=?"; 
-          $stmt = $db->prepare($sqlQ); 
-          $stmt->bind_param("i", $db_id); 
-          $db_id = $product_id; 
-          $stmt->execute(); 
-          $result = $stmt->get_result(); 
-          $productRow = $result->fetch_assoc(); 
-  
-          $itemData = array( 
-              'id' => $productRow['id'], 
-              'image' => $productRow['image'], 
-              'name' => $productRow['name'], 
-              'price' => $productRow['price'], 
-              'qty' => 1 
+          
         }
 
         public function updateCartItems() {
           // FILL HERE
-          $redirectURL = $insertItem?'viewCart.php':'index.php'; 
-    }elseif($_REQUEST['action'] == 'updateCartItem' && !empty($_REQUEST['id'])){ 
-        // Update item data in cart 
-        $itemData = array( 
-            'rowid' => $_REQUEST['id'], 
-            'qty' => $_REQUEST['qty'] 
-        ); 
-        $updateItem = $cart->update($itemData); 
+
         }
     }
 ?>
-
-// Ref : https://www.codexworld.com/simple-php-shopping-cart-using-sessions/
